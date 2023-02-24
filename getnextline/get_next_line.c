d4240aea6c17e6645320c48a7abe1ef5decf8a85
agallello <114653870+agallello@users.noreply.github.com>
2023-02-24 18:34:36 +0100

    Add files via upload
    tt

---
+++ getnextline/get_next_line.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agallell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 12:51:08 by agallell          #+#    #+#             */
+/*   Updated: 2023/02/22 10:45:16 by agallell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line"
+
+char	*ft_read_to_backup(int fd, char *backup)
+{
+	char	*buff;
+	int		bytes;
+
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(backup, '\n') && bytes != 0)
+	{
+		bytes = read(fd, buff, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[bytes] = '\0';
+		backup = ft_strjoin(backup, buff);
+	}
+	free(buff);
+	return (backup);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*backup;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	backup = ft_read_to_backup(fd, backup);
+	if (!backup)
+		return (NULL);
+	line = ft_get_line(backup);
+	backup = ft_backup(backup);
+	return (line);
+}
